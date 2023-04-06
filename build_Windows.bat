ed247a782b007a301913cb793b923695717a1297
tonnyMickael <tonnymickael2000@gmail.com>
2023-04-06 22:10:10 +0300

    rectificatif

---
+++ build_Windows.bat
@@ -0,0 +1,7 @@
+'C:\Program Files\Java\jdk-17.0.2\bin\jar.exe' cf frame.jar -C Framework\build\web\WEB-INF\classes\ etu2053
+
+REM Copy frame.jar utilisateur\build\web\WEB-INF\lib -Force
+
+REM 'C:\Program Files\Java\jdk-17.0.2\bin\jar.exe' cf teste.war -C Test_framework\build\web .
+
+ REM Copy teste.war 'C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps\' -Force
